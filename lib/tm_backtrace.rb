4deaa78f44253b6bc21277b26d0418c7d53cefa0
jugyo <jugyo.org@gmail.com>
2010-10-08 20:00:13 +0900

    cleaning backtrace

--- lib/tm_backtrace.rb
+++ lib/tm_backtrace.rb
@@ -3,11 +3,11 @@ class Exception
   def backtrace
     b = _backtrace
     return unless b
+
     b.map do |t|
-      file, line, _ = t.split(':')
-      " \e[90mtxmt://open/?url=file://\e[32m" +
-        File.expand_path(file) +
-        "\e[90m&line=\e[0m#{line} \e[31m#{_}\e[0m"
+      file, line, message = t.split(':')
+      file = File.expand_path(file).sub(File.expand_path('~'), '~')
+      " \e[90mtxmt://open/?url=file://\e[34m#{ file }\e[90m&line=\e[0m#{ line } \e[31m#{ message }\e[0m"
     end
   end
 end
